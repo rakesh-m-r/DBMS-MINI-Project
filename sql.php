d3f253d2ce8af9717e1e5f2559470c6aa05c0be8
Rakesh M R <49240813+rakesh-m-r@users.noreply.github.com>
2020-12-24 14:06:39 +0530

    Update sql.php

--- sql.php
+++ sql.php
@@ -1,6 +1,7 @@
 <?php 
-$host='sql204.epizy.com';
-$user='epiz_25149817';
-$project='epiz_25149817_project';
-$ps='rakesh1999';
+// replace *** with your database credentials
+$host='*****';
+$user='******';
+$project='******';
+$ps='******';
 ?>
