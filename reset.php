8c5f5c73c84dd0656f295f6a74c9fce1c1542081
Rakesh Mariyaplar <rakeshmr723@gmail.com>
2022-02-04 22:48:19 +0530

    fix:added new PHPMailer

--- reset.php
+++ reset.php
@@ -1,7 +1,7 @@
 <?php
-  require 'PHPMailer\src\PHPMailer.php';
-  require 'PHPMailer\src\SMTP.php';
-  require 'PHPMailer\src\Exception.php';
+  require 'PHPMailer/src/PHPMailer.php';
+  require 'PHPMailer/src/SMTP.php';
+  require 'PHPMailer/src/Exception.php';
  session_start(); ?>
 <html>
 <head>
